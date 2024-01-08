2626f021e884104b2d11943ece43a805ba86056b
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-08 23:36:47 +0530

    Adding the endpoint

---
+++ app/api/chat/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: "Hello, can you answer my questions.",
+    },
+    {
+      role: "model",
+      parts: "Sure, what do you want to know?",
+    },
+  ],
+  generationConfig: {
+    maxOutputTokens: 100,
+  },
+});
+
+export async function POST(req: NextRequest) {
+
+  // get prompt field from the request body
+  const reqBody = await req.json();
+
+  // get the userPrompt from the request body
+  const { userPrompt } = reqBody;
+
+  const result = await chat.sendMessage(userPrompt);
+
+  const response = await result.response;
+
+  const text = response.text();
+
+  return NextResponse.json({
+    text
+  });
+
+}
