9bf775406611fa4a42f724d10ce783acd5838ecb
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-09 17:23:27 +0530

    Adding persistance for the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import styles from "@/styles/root.module.css"
+import Image from "next/image"
+import Markdown from "react-markdown";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home() {
 
   const [prompt, setPrompt] = useState("");
 
+  const getDate = () => {
+    const date = new Date()
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  }
+
   const [chatHistory, setChatHistory] = useState([
-    { id: 1, message: 'Hello', sender: 'me', time: '12:00' },
+    { id: 1, message: 'Hello', sender: 'model', time: getDate() },
   ])
 
-  const RequestGemini = async (prompt:any) => {
+  const onPromptChange = (e: any) => {
+    localStorage.setItem('prompt', e.target.value)
+    setPrompt(e.target.value)
+  }
+
+  const RequestGemini = async (prompt: any) => {
     const response = await fetch('/api/chat', {
       method: 'POST',
       body: JSON.stringify({ userPrompt: prompt }),
@@ -18,39 +32,73 @@ export default function Home() {
         'Content-Type': 'application/json'
       }
     })
-  
     const data = await response.json()
-  
+
     setChatHistory((prevChatHistory) => [
       ...prevChatHistory,
-      { id: prevChatHistory.length + 1, message: data.text, sender: 'model', time: '12:00' }
+      { id: prevChatHistory.length + 1, message: data.text, sender: 'model', time: getDate() }
     ]);
+
+    setChatHistory((prevChatHistory) => {
+      const updatedChatHistory = [...prevChatHistory];
+      localStorage.setItem('chatHistory', JSON.stringify(updatedChatHistory));
+      return updatedChatHistory;
+    });
+
   }
 
+  // handle click
   const onHandleClick = async () => {
+    if (prompt == "") {
+      toast.error("Please enter the prompt")
+      return
+    }
 
-    console.log("clicked")
-    console.log(chatHistory.length)
-    setChatHistory([...chatHistory, { id: chatHistory.length + 1, message: prompt, sender: '', time: '12:00' }])
+    setChatHistory((prevChatHistory) => [
+      ...prevChatHistory,
+      { id: prevChatHistory.length + 1, message: prompt, sender: 'user', time: getDate() }
+    ]);
+
     await RequestGemini(prompt)
     setPrompt("")
   }
 
+  // show welcome message
+  useEffect(() => {
+    const visited = localStorage.getItem('visited')
+    if (!visited) {
+      toast.success("Welcome to Gemini")
+      setTimeout(() => {
+        toast.success("Get started by entering the prompt")
+      }, 2500)
+      localStorage.setItem('visited', "true")
+    }
+  })
 
+  useEffect(() => {
+    const chatHistory = localStorage.getItem('chatHistory')
+    if (chatHistory) {
+      setChatHistory(() => JSON.parse(chatHistory))
+      console.log(chatHistory)
+    }
+  }, [])
 
   return (
-    <main>
-      <div className="chat-history flex flex-col p-10">
+    <main className={styles.container}>
+      <Toaster />
+      <div className="flex gap-2">
+        <Image src="/chatbot.png" alt="logo" width={50} height={50} />
+        <h1>Gemini</h1>
+      </div>
+      <div className={`flex flex-col ${styles.chatHistory}`}>
         {chatHistory.length > 0 && chatHistory.map((chat) => (
-          <div key={chat.id} className={`chat-message ${chat.sender}`}>
-            <div className="chat-message-content">
-              <div className="chat-message-text" style={chat.id % 2 == 0 ? { float: 'left', backgroundColor: 'red', padding: '5px' } : { float: 'right', backgroundColor: 'green', padding: '5px' }}>{chat.message}</div>
-            </div>
+          <div key={chat.id} className="chat-message">
+            <Markdown className={`chat-message-text ${chat.sender == "user" ? styles.user : styles.model}`}>{chat.message}</Markdown>
           </div>
         ))}
       </div>
-      <div className="mt-10 flex justify-center items-center">
-        <input type="text" placeholder="Enter the prompt" value={prompt} className="p-5" onChange={(e) => setPrompt(e.target.value)} />
+      <div className={`flex justify-center items-center gap-5 ${styles.inputContainer}`}>
+        <input type="text" placeholder="Enter the prompt" value={prompt} className="p-5" onChange={onPromptChange} />
         <button onClick={onHandleClick}>click me</button>
       </div>
     </main>
